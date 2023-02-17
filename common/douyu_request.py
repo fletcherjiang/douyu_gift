e7bf4b64b76609e43170a6b1a093f0c1ae14b243
JIANG Yiyang <a@imjyy.com>
2023-02-17 16:58:50 +0800

    Update douyu_request.py

--- common/douyu_request.py
+++ common/douyu_request.py
@@ -14,7 +14,7 @@ class DYHTTPRequests:
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                           "Chrome/88.0.4324.182 Safari/537.36 Edg/88.0.705.81",
             "referer": "https://www.douyu.com",
-            "Cookie": self.cookie.encode('utf-8')
+            "Cookie": self.cookie.decode('utf-8')
         }
 
     def request(self, method, path, **kwargs):
