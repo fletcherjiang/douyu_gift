be419c0c787bfc5590d5d1eb35e6b5a7886be46e
JIANG Yiyang <a@imjyy.com>
2023-02-17 16:56:08 +0800

    Update douyu_request.py

--- common/douyu_request.py
+++ common/douyu_request.py
@@ -7,8 +7,7 @@ from common.get_secrets import get_secrets
 class DYHTTPRequests:
 
     def __init__(self):
-#         self.cookie = get_secrets('COOKIES')
-        self.cookie = "dy_did=f9f5c9ffc5be84b46faa895500031601; acf_did=f9f5c9ffc5be84b46faa895500031601; dy_did=f9f5c9ffc5be84b46faa895500031601; Hm_lvt_e99aee90ec1b2106afe7ec3b199020a7=1675346992,1675939362; PHPSESSID=j568f7sub8ufb6tpjug9lqie83; acf_avatar=https://apic.douyucdn.cn/upload/avatar_v3/202003/947f800e395d48b1b0941f6d04c9614e_; acf_auth=90beHcilm6o1JrCF777Lc5/hn1oHjX+1llHAeTtjhH0vPFhYGntZYi4C4rEM0mso50UVU85J0KooR1/NyZnltcPZP09/UlrqHNw7x7BsGJZBSvA2idVB0A; dy_auth=a3174yMm95UJxSkQSKSYM1UXbn5uRaKBNrE3eNtMVSJgRfbYNr9vmlPe3okdSb+4jmKjWuHb82JPss4ldZYml2ftZdjUnDKdRoCo7TOglj6aA8qVWsBmRQ; wan_auth37wan=c8d203080b7ch4gzAAJzg0jmBcE+36xCWEuvZIcCytsuvog3LJKX0F4kvXMUFrygDRbpKoUNzXEKq9zNbe95/F78zzNvAk8bbZD07YkoH3l8G3g; acf_uid=646956; acf_username=qq_sSXh6454; acf_nickname=完美小江; acf_own_room=1; acf_groupid=1; acf_phonestatus=1; acf_ct=0; acf_ltkid=45370323; acf_biz=1; acf_stk=da5737200db432a7; Hm_lpvt_e99aee90ec1b2106afe7ec3b199020a7=1676547889"
+        self.cookie = get_secrets('COOKIES')
         self.session = session()
         self.header = {
             "Content-Type": "application/x-www-form-urlencoded",
